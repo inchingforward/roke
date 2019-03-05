0a805202dae6fe02cf9bf754d894af21b9972471
Mike Janger <inchingforward@gmail.com>
2019-03-04 19:15:21 -0600

    Added rendering of text using Pixi.

--- src/App.fs
+++ src/App.fs
@@ -7,9 +7,46 @@ open Fable.Import.Browser
 let textInput = document.getElementById("tutorialText") :?> HTMLInputElement
 let display = document.getElementById("display") :?> HTMLDivElement
 
+let options = jsOptions<PIXI.ApplicationOptions> (fun o ->
+     o.backgroundColor <- Some 0x000000
+)
+
+let app = PIXI.Application(512., 512., options)
+display.appendChild(app.view) |> ignore
+
+// create a new Sprite from an image path
+let bunny = PIXI.Sprite.fromImage("fable_logo_small.png")
+
+let renderer : PIXI.WebGLRenderer = !!app.renderer
+
+// center the sprite's anchor point
+bunny.anchor.set(0.5)
+bunny.x <- renderer.width * 0.5
+bunny.y <- renderer.height * 0.5
+
+let textStyle = jsOptions<PIXI.TextStyle>( fun o ->
+    o.fontFamily<- !^"Arial"
+    o.fontSize<- !^36.
+    
+    o.fill <- "#ffffff"
+    o.strokeThickness<- 5.
+)
+
+let pixiText = PIXI.Text("", textStyle)
+pixiText.x <- 30.
+pixiText.y <- 30.
+
+app.stage.addChild pixiText |> ignore
+
 let textEntered text =
   console.log(text)
-  display.innerHTML <- text
+  //display.innerHTML <- text
+  //let pixiText = PIXI.Text(text, textStyle)
+  //pixiText.x <- 30.
+  //pixiText.y <- 30.
+
+  //app.stage.addChild pixiText |> ignore
+  pixiText.text <- text
   null
 
 textInput.addEventListener_keyup (fun e -> 
@@ -19,31 +56,3 @@ textInput.addEventListener_keyup (fun e ->
 )
 
 textInput.focus ()
-
-// let options = jsOptions<PIXI.ApplicationOptions> (fun o ->
-//     o.backgroundColor <- Some 0x000000
-// )
-
-// let app = PIXI.Application(400., 400., options)
-// document.body.appendChild(app.view) |> ignore
-
-// // create a new Sprite from an image path
-// let bunny = PIXI.Sprite.fromImage("fable_logo_small.png")
-
-// let renderer : PIXI.WebGLRenderer = !!app.renderer
-
-// // center the sprite's anchor point
-// //bunny.anchor.set(0.5)
-// //bunny.x <- renderer.width * 0.5
-// //bunny.y <- renderer.height * 0.5
-
-// app.stage.addChild(bunny) |> ignore
-
-//let tick delta =
-  // just for fun, let's rotate mr rabbit a little
-  // delta is 1 if running at 100% performance
-  // creates frame-independent tranformation
-  //bunny.rotation <- bunny.rotation + 0.1 * delta
-
-// Listen for animate update
-//app.ticker.add(tick) |> ignore
