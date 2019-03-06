09509857f77e266065617196c41ad0bac114702d
Mike Janger <mjanger@agilissystems.com>
2019-03-06 13:27:15 -0600

    Switched to using new graphics.

--- src/App.fs
+++ src/App.fs
@@ -5,7 +5,11 @@ open Fable.Import.Pixi
 open Fable.Import.Browser
 open Fable.Import.Pixi.PIXI
 
+let blockWidth = 64.
+let blockHeight = 100.
+
 let textInput = document.getElementById("tutorialText") :?> HTMLInputElement
+let testButton = document.getElementById("testButton") :?> HTMLButtonElement
 let display = document.getElementById("display") :?> HTMLDivElement
 
 let appOptions = jsOptions<PIXI.ApplicationOptions> (fun o ->
@@ -17,38 +21,76 @@ display.appendChild(app.view) |> ignore
 
 let renderer : PIXI.WebGLRenderer = !!app.renderer
 
-let roadEW = PIXI.Sprite.fromImage("tiles/roadEW.png")
+let grassTile = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile.x <- 100.
+grassTile.y <- 200.
 
-roadEW.x <- 100.
-roadEW.y <- 200.
+let grassTile2 = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile2.x <- grassTile.x + blockWidth
+grassTile2.y <- grassTile.y
 
-// height of tile: 100 x 58
-let roadEW2 = PIXI.Sprite.fromImage("tiles/roadEW.png")
-roadEW2.x <- roadEW.x + 48.
-roadEW2.y <- roadEW.y - 24.
+let grassTile3 = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile3.x <- grassTile2.x + blockWidth
+grassTile3.y <- grassTile2.y
 
-let grass = PIXI.Sprite.fromImage("tiles/grass.png")
-grass.x <- roadEW.x + 48.
-grass.y <- roadEW.y + 24.
+let grassTile4 = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile4.x <- grassTile3.x + blockWidth
+grassTile4.y <- grassTile3.y
 
-let grass2 = PIXI.Sprite.fromImage("tiles/grass.png")
-grass2.x <- roadEW.x + roadEW.x - 4.
-grass2.y <- roadEW.y
+let waterTile1 = PIXI.Sprite.fromImage("blocks/tileWater_1.png")
+waterTile1.x <- grassTile.x
+waterTile1.y <- grassTile.y - blockWidth
 
-let crossRoadESW = PIXI.Sprite.fromImage("tiles/crossroadESW.png")
-crossRoadESW.x <- roadEW.x + roadEW.x - 4.
-crossRoadESW.y <- roadEW.y - 48.
+let waterTile2 = PIXI.Sprite.fromImage("blocks/tileWater_3.png")
+waterTile2.x <- waterTile1.x + blockWidth
+waterTile2.y <- waterTile1.y
 
-let endN = PIXI.Sprite.fromImage("tiles/endN.png")
-endN.x <- roadEW2.x + 96.
-endN.y <- roadEW2.y
+let waterTile3 = PIXI.Sprite.fromImage("blocks/tileWater_3.png")
+waterTile3.x <- waterTile2.x + blockWidth
+waterTile3.y <- waterTile2.y
 
-app.stage.addChild crossRoadESW |> ignore
-app.stage.addChild roadEW2 |> ignore
-app.stage.addChild roadEW |> ignore
-app.stage.addChild endN |> ignore
-app.stage.addChild grass2 |> ignore
-app.stage.addChild grass |> ignore
+let waterTile4 = PIXI.Sprite.fromImage("blocks/tileWater_1.png")
+waterTile4.x <- waterTile3.x + blockWidth
+waterTile4.y <- waterTile3.y
+
+let grassTile5 = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile5.x <- waterTile1.x
+grassTile5.y <- waterTile1.y - blockWidth
+
+let grassTile6 = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile6.x <- grassTile5.x + blockWidth
+grassTile6.y <- grassTile5.y
+
+let grassTile7 = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile7.x <- grassTile6.x + blockWidth
+grassTile7.y <- grassTile6.y
+
+let grassTile8 = PIXI.Sprite.fromImage("blocks/tileGrass.png")
+grassTile8.x <- grassTile7.x + blockWidth
+grassTile8.y <- grassTile7.y
+
+let wizard = PIXI.Sprite.fromImage("blocks/character_wizard.png")
+wizard.x <- grassTile.x 
+wizard.y <- grassTile.y - 48.
+
+// Must be built from top down
+app.stage.addChild grassTile5 |> ignore
+app.stage.addChild grassTile6 |> ignore
+app.stage.addChild grassTile7 |> ignore
+app.stage.addChild grassTile8 |> ignore
+
+app.stage.addChild waterTile1 |> ignore
+app.stage.addChild waterTile2 |> ignore
+app.stage.addChild waterTile3 |> ignore
+app.stage.addChild waterTile4 |> ignore
+
+app.stage.addChild grassTile |> ignore
+app.stage.addChild grassTile2 |> ignore
+app.stage.addChild grassTile3 |> ignore
+app.stage.addChild grassTile4 |> ignore
+
+app.stage.addChild wizard |> ignore
+
 
 let textStyle = jsOptions<PIXI.TextStyle>( fun o ->
     o.fontFamily<- !^"Arial"
@@ -63,6 +105,11 @@ pixiText.x <- 30.
 pixiText.y <- 30.
 
 app.stage.addChild pixiText |> ignore
+
+testButton.addEventListener_click (fun e ->
+  wizard.x <- wizard.x + blockWidth
+  null
+)
 
 let textEntered text =
   pixiText.text <- text
